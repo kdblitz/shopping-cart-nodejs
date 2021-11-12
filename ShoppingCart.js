3beadafaef5cf2e07a6267b13131ff5348664f58
Ken Darione Lee <kdblitz@gmail.com>
2021-11-13 01:16:10 +0800

    Refactor cartdata structure to prepare on supporting promos

--- ShoppingCart.js
+++ ShoppingCart.js
@@ -1,10 +1,11 @@
 const formatter = require('./utils/formatter');
 
-const ShoppingCart = class {
+class ShoppingCart {
   productMap = {};
   addedItems = {};
   promoCode = null;
   toCurrencyFormat = (val) => val;
+  applyPromos = [(cartData, productMap) => cartData];
 
   constructor({
     products = [],
@@ -20,7 +21,7 @@ const ShoppingCart = class {
     this.addedItems = {};
   }
 
-  add(itemCode, promoCode = "") {
+  add(itemCode, promoCode = null) {
     if (this.productMap[itemCode]) {
       this.addedItems[itemCode] = this.addedItems[itemCode]
         ? ++this.addedItems[itemCode]
@@ -28,22 +29,49 @@ const ShoppingCart = class {
     } else {
       console.error(`${itemCode} not found`);
     }
+    this.promoCode = promoCode ? promoCode : this.promoCode;
+  }
+
+  _prepareCartData() {
+    const cartItems = Object.entries(this.addedItems).reduce(
+      (cartData, [productCode, quantity]) => {
+        const { name, price } = this.productMap[productCode];
+        cartData[productCode] = {
+          item: name,
+          price,
+          quantity,
+          subtotal: price * quantity,
+        };
+        return cartData;
+      },
+      {}
+    );
+
+    return {
+      cartItems,
+      promoCode: this.promoCode,
+    };
+  }
+
+  applyPromosToItems() {
+    return this._prepareCartData();
   }
 
   get total() {
-    const total = Object.entries(this.addedItems).reduce(
-      (totalAmount, [productCode, quantity]) => {
-        totalAmount += this.productMap[productCode].price * quantity;
-        return totalAmount;
-      },
-      0
-    );
+    const cartData = this.applyPromosToItems();
+
+    const total = Object.values(cartData.cartItems)
+      .map((cartItem) => cartItem.subtotal)
+      .reduce((sum, subtotal) => sum + subtotal, 0);
+
     return this.toCurrencyFormat(total);
   }
 
   get items() {
-    return Object.entries(this.addedItems).map(([productCode, quantity]) => ({
-      item: this.productMap[productCode].name,
+    const cartData = this.applyPromosToItems();
+
+    return Object.values(cartData.cartItems).map(({ item, quantity }) => ({
+      item,
       quantity,
     }));
   }
--- utils/formatter.js
+++ utils/formatter.js
@@ -2,9 +2,7 @@ const toCurrencyFormat = function (val) {
   const absVal = Math.abs(val);
   if (Number.isNaN(absVal)) return NaN;
 
-  return val >= 0
-    ? `$${Number(absVal).toFixed(2)}`
-    : `-$${Number(absVal).toFixed(2)}`;
+  return val >= 0 ? `$${absVal.toFixed(2)}` : `-$${absVal.toFixed(2)}`;
 }
 
 module.exports = {
