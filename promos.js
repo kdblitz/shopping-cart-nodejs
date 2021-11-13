457c9e746e68b52ce65b8423a5146b9c42f49589
Ken Darione Lee <kdblitz@gmail.com>
2021-11-13 17:14:56 +0800

    Implement 3 for 2 discount for unli 1gb product

--- promos.js
+++ promos.js
@@ -23,7 +23,29 @@ const clone = (data) => {
   return JSON.parse(JSON.stringify(data));
 };
 
-const ult_small_3for2_promo = (cart) => cart;
+const ult_small_3for2_promo = (cartData) => {
+  if (!cartData.cartItems.ult_small) {
+    return cartData;
+  }
+  cartData = clone(cartData);
+
+  const quantityToPay = 2;
+  const discountThreshold = 3;
+  const { quantity, price, ...data } = cartData.cartItems.ult_small;
+  const discountedItems = Math.floor(quantity / discountThreshold);
+  const regularItems = quantity % discountThreshold;
+
+  const discountedAmount = discountedItems * quantityToPay * price;
+  const regularAmount = regularItems * price;
+  cartData.cartItems.ult_small = {
+    quantity,
+    price,
+    ...data,
+    subtotal: discountedAmount + regularAmount,
+  };
+
+  return cartData;
+};
 const ult_medium_1gb_free_promo = (cart, productMap) => cart;
 
 const ult_large_bulk_promo = (cartData) => {
--- test/promos.test.js
+++ test/promos.test.js
@@ -5,6 +5,71 @@ const { promos, prepareCartData } = require("./helper");
 
 let toTestPromo = null;
 
+
+describe("Unlimited 1GB 3 for 2 code promo", () => {
+  before(() => {
+    toTestPromo = promos.ult_small_3for2_promo;
+  });
+
+  it("should not grant discount for single Unlimited 1GB purchase", () => {
+    let cartData = prepareCartData({ ult_small: 1 });
+    let tranformedCartData = toTestPromo(cartData);
+    expect(tranformedCartData).to.be.deep.equal(cartData);
+  });
+
+  it("should grant discount for 3 Unlimited 1GB purchase", () => {
+    let cartData = prepareCartData({ ult_small: 3 });
+    let tranformedCartData = toTestPromo(cartData);
+    expect(tranformedCartData).to.be.deep.equal({
+      cartItems: {
+        ult_small: {
+          item: "Unlimited 1GB",
+          price: 24.9,
+          quantity: 3,
+          subtotal: 49.8,
+        },
+      },
+      promoCode: null,
+    });
+  });
+
+  it("should partially grant discount for 5 Unlimited 1GB purchase", () => {
+    let cartData = prepareCartData({ ult_small: 5 });
+    let tranformedCartData = toTestPromo(cartData);
+    const quantityToPay = 4; // 1 item will be discounted out of 5
+
+    expect(tranformedCartData).to.be.deep.equal({
+      cartItems: {
+        ult_small: {
+          item: "Unlimited 1GB",
+          price: 24.9,
+          quantity: 5,
+          subtotal: 24.9 * quantityToPay,
+        },
+      },
+      promoCode: null,
+    });
+  });
+
+  it("should grant 2 discounts for 6 Unlimited 1GB purchase", () => {
+    let cartData = prepareCartData({ ult_small: 6 });
+    let tranformedCartData = toTestPromo(cartData);
+    const quantityToPay = 4; // 1 item will be discounted out of 5
+
+    expect(tranformedCartData).to.be.deep.equal({
+      cartItems: {
+        ult_small: {
+          item: "Unlimited 1GB",
+          price: 24.9,
+          quantity: 6,
+          subtotal: 24.9 * quantityToPay,
+        },
+      },
+      promoCode: null,
+    });
+  });
+});
+
 describe("Unlimited 5GB bulk order promo", () => {
   before(() => {
     toTestPromo = promos.ult_large_bulk_promo;
